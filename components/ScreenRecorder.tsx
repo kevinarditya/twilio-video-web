a136ca5da11ca6772a152ba1ccdc8c163fbc437a
Kevin Arditya <kevin.arditya@gmail.com>
2023-02-27 02:27:03 +0700

    [kevin] handle video recorder stopwatch timer

--- components/ScreenRecorder.tsx
+++ components/ScreenRecorder.tsx
@@ -1,7 +1,8 @@
 import { PlayCircleFilled, StopCircle } from '@mui/icons-material';
-import { Button } from '@mui/material';
-import React, { useCallback } from 'react';
+import { Button, Snackbar } from '@mui/material';
+import React, { useCallback, useState } from 'react';
 import { Recorder, useRecorderPermission } from '../hooks/useRecorderPermission';
+import Timer from './Timer';
 
 type ScreenRecorderProps = {
   audioTracks: Array<MediaStreamTrack>
@@ -10,15 +11,29 @@ type ScreenRecorderProps = {
 
 export default function ScreenRecorder({ audioTracks, addRecorder }: ScreenRecorderProps) {
   const recorder: Recorder = useRecorderPermission(audioTracks, 'video');
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const handleCloseSnackbar = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const handleToggleRecording = useCallback(async () => {
     if (!recorder.isRecording) {
       if (recorder) {
         recorder.startRecord();
+        setMessage('Start Screen Recording');
+        setOpen(true);
       }
     } else {
       if (recorder) {
         const recorderFileBlob = await recorder.stopRecord();
+        setMessage('Stop Screen Recording');
+        setOpen(true);
         addRecorder(recorderFileBlob);
       }
     }
@@ -32,8 +47,15 @@ export default function ScreenRecorder({ audioTracks, addRecorder }: ScreenRecor
         sx={{ backgroundColor: recorder.isRecording ? 'white' : '' }}
         onClick={handleToggleRecording}
       >
-        {recorder.isRecording ? 'Stop Recording' : 'Start Video Recording'}
+        {recorder.isRecording ? <Timer time={recorder.time}/> : 'Screen Recording'}
       </Button>
+      <Snackbar
+        open={open}
+        autoHideDuration={4000}
+        onClose={handleCloseSnackbar}
+        message={message}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      />
     </>
   );
 };
